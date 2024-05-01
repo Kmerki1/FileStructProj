e7664d8c9389d82006b1ecd47b9ab18587fa2d69
ghuber2 <161767959+ghuber2@users.noreply.github.com>
2024-05-01 17:13:52 -0400

    Update FileObject.java

--- FileObject.java
+++ FileObject.java
@@ -1,14 +1,19 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileObject implements AbstractFile {
     String name;
 
+    File f1;
+
     public FileObject(String name) {
+        this.name = name;
         try {
-            File myObj = new File(name);
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
+            f1 = new File(name);
+            if (f1.createNewFile()) {
+                System.out.println("File created: " + f1.getName());
             } else {
                 System.out.println("File already exists.");
             }
@@ -18,7 +23,28 @@ public class FileObject implements AbstractFile {
         }
     }
 
+    public void writeTo() {
+        try {
+            FileWriter myWriter = new FileWriter(name);
+            Scanner input = new Scanner(System.in);
+            String userInput = input.nextLine();
+
+            myWriter.write(userInput);
+
+            myWriter.close();
+            input.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void ls() {
         System.out.println("File: " + name);
+    }
+
+    @Override
+    public String getName() {
+        return name;
     }
 }
