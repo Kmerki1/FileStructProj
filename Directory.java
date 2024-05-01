89e98ad080bfc9fc22eacd6dcd0e620906ee0a04
ghuber2 <161767959+ghuber2@users.noreply.github.com>
2024-05-01 16:42:59 -0400

    Update Directory.java

--- Directory.java
+++ Directory.java
@@ -1,9 +1,18 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 public class Directory implements AbstractFile{
     String name;
     ArrayList<AbstractFile> includedFiles = new ArrayList<AbstractFile>();
     public Directory(String name){
-        this.name = name;
+        File f1 = new File(name);  
+      //Creating a folder using mkdir() method  
+      boolean bool = f1.mkdir();  
+      if(bool){  
+         System.out.println("Directory created: " + name);  
+      }else{  
+         System.out.println("Error Found!");  
+      }  
     }
 
     public void ls(){
