4efdb7d03dfb6825c3305e1781c15ad0c76d191a
ghuber2 <161767959+ghuber2@users.noreply.github.com>
2024-05-01 16:45:20 -0400

    Update Directory.java

--- Directory.java
+++ Directory.java
@@ -1,27 +1,28 @@
 import java.io.File;
-import java.io.IOException;
 import java.util.*;
-public class Directory implements AbstractFile{
+
+public class Directory implements AbstractFile {
     String name;
     ArrayList<AbstractFile> includedFiles = new ArrayList<AbstractFile>();
-    public Directory(String name){
-        File f1 = new File(name);  
-      //Creating a folder using mkdir() method  
-      boolean bool = f1.mkdir();  
-      if(bool){  
-         System.out.println("Directory created: " + name);  
-      }else{  
-         System.out.println("Error Found!");  
-      }  
+
+    public Directory(String name) {
+        File f1 = new File(name);
+        boolean bool = f1.mkdir();
+        if (bool) {
+            System.out.println("Directory created: " + name);
+        } else {
+            System.out.println("Directory "+ name +" already exists");
+        }
     }
 
-    public void ls(){
+    public void ls() {
         System.out.println("Directory: " + name);
-        for(int i = 0; i < includedFiles.size(); i++){
+        for (int i = 0; i < includedFiles.size(); i++) {
             includedFiles.get(i).ls();
         }
     }
-    public void add(AbstractFile file){
+
+    public void add(AbstractFile file) {
         includedFiles.add(file);
     }
 }
