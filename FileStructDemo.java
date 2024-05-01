d6867c57909be0e706687df7ef24077121f60509
ghuber2 <161767959+ghuber2@users.noreply.github.com>
2024-05-01 16:33:36 -0400

    Update FileStructDemo.java

--- FileStructDemo.java
+++ FileStructDemo.java
@@ -1,19 +1,23 @@
 public class FileStructDemo{
     //Kyle Merkins
     public static void main(String[] args){
+        
+        FileObject file1 = new FileObject("file1.txt");
+        
         Directory test = new Directory("root");
         test.ls();//shows just the root currently
         System.out.println();//spacer
 
         Directory test2 = new Directory("subDirectory1");
         test.add(test2);
-        File testFile = new File("testFile");
+        FileObject testFile = new FileObject("testFile");
         test.add(testFile);
         test.ls();//shows root, a subdirectory, and a file
         System.out.println();//spacer
 
         test.add(new Directory("Subdirectory2"));
-        test2.add(new File("subDirectory1's file"));
+        test2.add(new FileObject("subDirectory1's file"));
         test.ls();//shows root, a subdirectory and its inner file, a file and another directory
+        
     }
 }
